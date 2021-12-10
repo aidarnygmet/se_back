50ae28e86723107d636912fe2e2995cc32723fc5
aidarnygmet <aidarn198@gmail.com>
2021-12-10 21:51:16 +0600

    21:51

--- src/main/java/com/example/seproject/se_back/RoomController.java
+++ src/main/java/com/example/seproject/se_back/RoomController.java
@@ -1,9 +1,22 @@
 package com.example.seproject.se_back;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RoomController {
-
+    @Autowired
+    private RoomRepo roomRepo;
+    @CrossOrigin(origins = "http://localhost:8080")
+    @GetMapping("/room")
+    public List<Room> getRoom(){
+        List<Room> listroom=roomRepo.findAll();
+        return listroom;
+    }
 }
